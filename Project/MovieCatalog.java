784adb756ce130dd267336e4f406063e4d362c79
raymon mando <mandor@sheridancollege.ca>
2022-03-31 20:53:49 -0400

    updated

--- Project/MovieCatalog.java
+++ Project/MovieCatalog.java
@@ -1,6 +1,10 @@
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,8 +15,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -33,11 +39,11 @@ public class MovieCatalog extends Application {
                 "Forrest Gump", "Fight Club", "Pulp Fiction", "Star Wars: Episode V", "WALL-E",
                 "Spider-Man: No Way Home", "Toy Story", "Up");
         ListView<String> listView = new ListView<String>(movies);
-        listView.setMaxSize(200, 160);
+        listView.setMaxSize(160, 300);
 
         // Layout
         VBox layout = new VBox(10);
-        layout.setPadding(new Insets(50, 5, 5, 20));
+        layout.setPadding(new Insets(50, 0, 0, 20));
         layout.getChildren().addAll(label, listView);
         layout.setStyle("-fx-background-color: WHITE");
 
@@ -46,7 +52,7 @@ public class MovieCatalog extends Application {
         addNew.setText("Add New"); // Button text
         // Location of Button
         addNew.setTranslateX(480);
-        addNew.setTranslateY(-185);
+        addNew.setTranslateY(-60);
         layout.getChildren().addAll(addNew);
 
         // Create Edit button
@@ -54,7 +60,7 @@ public class MovieCatalog extends Application {
         Edit.setText("Edit"); // Button text
         // Location of Button
         Edit.setTranslateX(480);
-        Edit.setTranslateY(-190);
+        Edit.setTranslateY(-40);
         layout.getChildren().addAll(Edit);
 
         // Create Search button
@@ -62,7 +68,7 @@ public class MovieCatalog extends Application {
         Search.setText("Search"); // Button text
         // Location of Button
         Search.setTranslateX(480);
-        Search.setTranslateY(-195);
+        Search.setTranslateY(-20);
         layout.getChildren().addAll(Search);
 
         // Create Delete button
@@ -70,8 +76,25 @@ public class MovieCatalog extends Application {
         Delete.setText("Delete"); // Button text
         // Location of Button
         Delete.setTranslateX(480);
-        Delete.setTranslateY(-200);
+        Delete.setTranslateY(0);
         layout.getChildren().addAll(Delete);
+
+        //Text
+        Label movieInfo = new Label("Movie Information");
+        layout.getChildren().addAll(movieInfo);
+        movieInfo.setTranslateX(190);
+        movieInfo.setTranslateY(-250);
+        movieInfo.setFont(Font.font("verdance", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
+        //Genre
+        Label genre = new Label("Genre");
+        TextField textField = new TextField();
+        layout.getChildren().addAll(genre, textField);
+        textField.setTranslateX(180);
+        textField.setTranslateY(-200);
+        textField.setPrefWidth(150);
+        textField.setMaxWidth(150);
+        layout.setSpacing(10);
 
         // Stage
         Scene scene = new Scene(layout, 595, 300);
